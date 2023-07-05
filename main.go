7a7c8db3a4cd9fbaf3d6385b7fcb460c44dacd48
Ihor Bobulych <ihbobuacc@gmail.com>
2023-07-05 13:03:02 +0300

    Handle id from url query

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Create a handler that writes a slice of bytes contains text "Hello from App" as response body.
 func home(w http.ResponseWriter, r *http.Request) {
+	// Handle only root path, but do not handle other arbitrary paths
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from App"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Showing a snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Showing the note with ID: %d...", id)
 }
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method is forbidden!", 405)
+		return
+	}
 	w.Write([]byte("Creating a snippet..."))
 }
 func main() {
